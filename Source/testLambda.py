a3a6f9b4c33dce7ff207e41d4917000686d84e45
kailash.malvia <kailash.malvia@fieldequip.com>
2024-12-30 18:49:49 +0530

    audit

--- Source/testLambda.py
+++ Source/testLambda.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! First Commit change for audit report')
+        'body': json.dumps('audit report')
     }
